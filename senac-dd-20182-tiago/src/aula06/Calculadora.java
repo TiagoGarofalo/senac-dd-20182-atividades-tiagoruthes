1c70e24d009f38766f1ce3f06c8b1d5437c75e25
pepi <pepi@DESKTOP-848S4AU.home>
2018-08-22 12:55:43 -0300

    Criada tela e calculadora.

--- senac-dd-20182-tiago/src/aula06/Calculadora.java
+++ senac-dd-20182-tiago/src/aula06/Calculadora.java
@@ -6,10 +6,19 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import javax.swing.JButton;
+import java.awt.Insets;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
-public class PrimeiroJFrame extends JFrame {
+public class Calculadora extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textField;
 
 	/**
 	 * Launch the application.
@@ -18,7 +27,7 @@ public class PrimeiroJFrame extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					PrimeiroJFrame frame = new PrimeiroJFrame();
+					Calculadora frame = new Calculadora();
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -30,13 +39,77 @@ public class PrimeiroJFrame extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public PrimeiroJFrame() {
+	public Calculadora() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		textField = new JTextField();
+		textField.setBounds(5, 5, 424, 20);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		JButton btnSoma = new JButton("+");
+		btnSoma.setBounds(340, 51, 89, 23);
+		contentPane.add(btnSoma);
+		
+		JButton btnSubtra = new JButton("-");
+		btnSubtra.setBounds(340, 85, 89, 23);
+		contentPane.add(btnSubtra);
+		
+		JButton btnDiv = new JButton("/");
+		btnDiv.setBounds(340, 150, 89, 23);
+		contentPane.add(btnDiv);
+		
+		JButton btnMult = new JButton("*");
+		btnMult.setBounds(340, 116, 89, 23);
+		contentPane.add(btnMult);
+		
+		JButton btn7 = new JButton("7");
+		btn7.setBounds(10, 51, 89, 23);
+		contentPane.add(btn7);
+		
+		JButton btn8 = new JButton("8");
+		btn8.setBounds(109, 51, 89, 23);
+		contentPane.add(btn8);
+		
+		JButton btn5 = new JButton("5");
+		btn5.setBounds(109, 85, 89, 23);
+		contentPane.add(btn5);
+		
+		JButton btn4 = new JButton("4");
+		btn4.setBounds(10, 85, 89, 23);
+		contentPane.add(btn4);
+		
+		JButton btn9 = new JButton("9");
+		btn9.setBounds(208, 51, 89, 23);
+		contentPane.add(btn9);
+		
+		JButton btn1 = new JButton("1");
+		btn1.setBounds(10, 116, 89, 23);
+		contentPane.add(btn1);
+		
+		JButton btn2 = new JButton("2");
+		btn2.setBounds(109, 116, 89, 23);
+		contentPane.add(btn2);
+		
+		JButton btn6 = new JButton("6");
+		btn6.setBounds(208, 85, 89, 23);
+		contentPane.add(btn6);
+		
+		JButton btn3 = new JButton("3");
+		btn3.setBounds(208, 116, 89, 23);
+		contentPane.add(btn3);
+		
+		JButton btn0 = new JButton("0");
+		btn0.setBounds(109, 150, 89, 23);
+		contentPane.add(btn0);
+		
+		JButton btnResult = new JButton("=");
+		btnResult.setBounds(340, 227, 89, 23);
+		contentPane.add(btnResult);
 	}
-
 }
--- senac-dd-20182-tiago/src/aula06/PrimeiraTela.java
+++ senac-dd-20182-tiago/src/aula06/PrimeiraTela.java
@@ -3,10 +3,19 @@ package aula06;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JButton;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+
+import javax.swing.JToolBar;
+import javax.swing.JLabel;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Color;
 
 public class PrimeiraTela {
 
-	private JFrame frame;
+	private JFrame frmPrimeira;
 
 	/**
 	 * Launch the application.
@@ -16,7 +25,7 @@ public class PrimeiraTela {
 			public void run() {
 				try {
 					PrimeiraTela window = new PrimeiraTela();
-					window.frame.setVisible(true);
+					window.frmPrimeira.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -35,9 +44,22 @@ public class PrimeiraTela {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmPrimeira = new JFrame();
+		frmPrimeira.getContentPane().setBackground(Color.LIGHT_GRAY);
+		frmPrimeira.setTitle(" Primeira Tela");
+		frmPrimeira.setBounds(100, 100, 450, 300);
+		frmPrimeira.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		JButton confirmButton = new JButton("Confirmar");
+		confirmButton.setPreferredSize(new Dimension(20, 25));
+		confirmButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		frmPrimeira.getContentPane().add(confirmButton, BorderLayout.SOUTH);
+
+		JLabel lbNomeUsuario = new JLabel("Nome Usuário");
+		frmPrimeira.getContentPane().add(lbNomeUsuario, BorderLayout.NORTH);
 	}
 
 }
